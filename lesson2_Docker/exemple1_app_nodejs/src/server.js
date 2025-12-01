f4d7e3873530193ecfc3ff2fcedccaa8f39eb3fe
hibanajjar998 <hiba.najjar4@etu.univ-lorraine.fr>
2025-12-01 10:32:12 +0000

    add application examples

---
+++ lesson2_Docker/exemple1_app_nodejs/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome to my awesome app!</h1>
+    <img src="https://i.pinimg.com/originals/cb/22/60/cb22603604e36d1caab7cc4e729506c9.gif" alt="Beautiful Landscape" style="max-width:400px;">
+  `);
+});
+
+app.listen(8088, function () {
+  console.log("app listening on port 8088, go to http://localhost:8088");
+});
