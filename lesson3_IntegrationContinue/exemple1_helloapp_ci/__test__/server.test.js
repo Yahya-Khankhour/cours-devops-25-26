ee647ff483330d44801dd0bb0bbb19fec2a8f8a9
hibanajjar998 <hiba.najjar4@etu.univ-lorraine.fr>
2025-12-08 09:17:18 +0000

    add lesson3 first exemple

---
+++ lesson3_IntegrationContinue/exemple1_helloapp_ci/__test__/server.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../src/server'); // Importe l'application Express exportée
+
+describe('GET /', () => {
+  it('devrait répondre avec un statut 200 et contenir le titre', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    // On vérifie que le corps de la réponse contient le titre de la page
+    expect(response.text).toContain('<h1>Welcome to my awesome app!</h1>');
+  });
+});
